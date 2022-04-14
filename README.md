17b34111b9033b4c2356ece35ff01b8cc4dc030e
archaic10 <vitor.franca.9400@gmail.com>
2022-04-14 10:16:02 -0300

    feat: create skeleton

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
+![img](https://github.com/madeiramadeirabr/action-check-status-sd-jira/blob/production/img/action-sync-package-version-with-tag.svg)
 # action-sync-package-version-with-tag
+
+## Description
+Esta action procura a versão lançada da tag recentemente no repositório e verifica se ela atende aos seguintes padrões:
+
+```
+- v1.0.0.1
+- v1.0.0
+- 1.0.0.1
+- 1.0.0
+```
+
+## Contexto de negócio:
+Essa action é utilizada em conjunto com [`action-generate-release`](https://github.com/madeiramadeirabr/generate-release 'action-generate-release') para realizar o processo de geração de changelog
+## Squad:
+[SRE Team](https://github.com/orgs/madeiramadeirabr/teams/team-platform-services 'SRE Team')
+
+## Requisitos:
+- O projeto deve ser em node
+
+## Exemplo de uso 
+
+```yml
+uses: madeiramadeirabr/action-sync-package-version-with-tag@production
+with:
+    github-token: ${{ secrets.GITHUB_TOKEN }}
+    branch: development
+```
+### Or
+
+```yml
+uses: madeiramadeirabr/action-sync-package-version-with-tag@production
+with:
+    github-token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+> Esta action também altera a versão do arquivo `package.json` da branch especificada, se nenhuma branch for especificada, ela alterará o arquivo de branch padrão
---
+++ index.js
@@ -0,0 +1,161 @@
+const { Octokit } = require("@octokit/core")
+const github = require('@actions/github')
+const core = require('@actions/core');
+const base64 = require('base-64')
+const axios = require('axios')
+const githubToken = core.getInput('github-token')
+const octokit = new Octokit({ auth: githubToken})
+const branch = core.getInput('branch')
+
+async function run(){
+    if(githubToken){
+        try{
+            let newVersion = await getTag()
+            setVersion(newVersion)
+        }catch(error){
+            core.setFailed('A branch ou arquivo não existe!')
+        }
+    }else{
+        core.setFailed('O parâmetro github-token é obrigatório!')
+    }
+}
+
+async function findTag(){
+    let param = {
+        owner: github.context.payload.repository.owner.name,
+        repo: github.context.payload.repository.name
+    }
+    return octokit.request('GET /repos/{owner}/{repo}/git/refs/tags', param)
+}
+
+async function getTag(){
+    let numberTag = await findTag()
+
+    if(numberTag.status == 200){
+        let lastTag = numberTag.data.pop().ref.split('/').pop()
+        console.log('A tag encontrada é', lastTag)
+        if(!validateTag(lastTag)){
+            core.setFailed(`A tag ${lastTag} não é uma tag válida!`)
+            return false
+        }else{            
+            return lastTag
+        }            
+    }else{
+        core.setFailed("Nenhuma tag foi definida para seu projeto. Defina uma tag e execute a ação novamente")
+        return false
+    }
+}
+
+function validateTag(tag){
+    let defaulTag = tag.match('([v0-9|0-9]+).([0-9]+).([0-9]+).([0-9]+)')
+    
+    if(defaulTag){
+        return tag
+    }
+    
+    defaulTag = tag.match('([v0-9|0-9]+).([0-9]+).([0-9]+)')
+    if(defaulTag){
+        return tag
+    }
+    
+    return false
+}
+
+
+async function setVersion(newVersion){
+    let content = await getContent()
+    let {sha} = content.data
+    let {download_url} = content.data
+    if (download_url){
+        let {data} = await getContentFile(download_url)
+        modifyVersionAndUploadFile(data, sha, newVersion)
+    }
+}
+
+function modifyVersionAndUploadFile(data, sha, newVersion){
+    if (data && data != ''){
+        if(modifyVersion(data, newVersion) && modifyVersion(data, newVersion) != ''){
+            let newFile = modifyVersion(data, newVersion)
+            let fileBase64 = base64.encode(JSON.stringify(newFile))
+            uploadGithub(fileBase64, 'package.json', sha)
+        }else{
+            core.setFailed('Falha ao atualizar a versão do package.json!')
+        }
+    }else{
+        core.setFailed('Falha ao ler o arquivo!')
+    }
+}
+
+function getContent(){
+    let param = {
+        owner: github.context.payload.repository.owner.name,
+        repo: github.context.payload.repository.name,
+        path: 'package.json',
+    }
+    if (branch && branch != ''){
+        param['ref'] = branch 
+    }
+    return  octokit.request('GET /repos/{owner}/{repo}/contents/{path}', param, (response)=>{
+        if (response.status  == 200){
+            return response
+        }
+
+        return false
+    })
+}
+
+async function getContentFile (raw_url){
+    
+    return axios.get(raw_url, {
+        headers: {
+            Authorization: `Bearer ${githubToken}`
+        }
+    })
+}
+
+function modifyVersion (package_json_obj, newVersion){
+    if(newVersion == '' || newVersion == undefined){
+        console.log('É necessária uma nova versão!')
+        return false
+    }
+    package_json_obj.version = newVersion
+    return package_json_obj
+}
+
+async function uploadGithub(content, fileName, sha){
+    let param = {
+        owner: github.context.payload.repository.owner.name,
+        repo: github.context.payload.repository.name,
+        path: 'package.json',
+        message: `ci: Update ${fileName}`,
+        content: content,
+        sha: sha
+    }
+    
+    uploadFileBase64(param, fileName)
+}
+
+async function uploadFileBase64(param, fileName){
+    if (branch && branch != ''){
+        delete param.ref
+        param['branch'] = branch 
+    }
+    await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', param).then(()=>{
+        
+        let message = `Arquivo ${fileName} atualizado`
+        console.log({
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+            },
+            'body': {
+                'message': message,
+            }
+        })
+        core.setOutput("success", message)
+        
+    }).catch(function(error){
+        core.setFailed("Error ao commitar file: ",error)
+    })
+}
+run()
